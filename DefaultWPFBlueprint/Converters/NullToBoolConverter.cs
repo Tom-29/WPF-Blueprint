f9ed3544b2d2d0e1ab5d5a6b3c399dfd1ccd0bda
Tom Jegge <tom.jegge@post.ch>
2025-03-07 13:45:49 +0100

    feat: add null to bool converter

---
+++ DefaultWPFBlueprint/Converters/NullToBoolConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace DefaultWPFBlueprint.Converters;
+
+public class NullToBoolConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value != null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return boolValue ? new object() : null;
+        }
+
+        return null;
+    }
+}
